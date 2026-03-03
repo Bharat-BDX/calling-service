008ceca0372cddff7403fce48042a4d92f86708f
Bharat Namdev <bharat@recare.ai>
2026-03-03 11:11:27 +0000

    testing the call state person updation

--- src/services/state-update.service.js
+++ src/services/state-update.service.js
@@ -104,64 +104,40 @@ async function finalizeAttemptAndUpdateState(payload) {
 
       console.log('SCRUBBED_CALL_ANALYSIS', JSON.stringify(payload.call.scrubbed_call_analysis));
 
-      await client.query('COMMIT');
-
     } else if (payload.event === "call_ended") {
       console.log('CALL_ENDED_EVENT');
     }else {
       console.log('SKIPPING_EVENT', payload.event);
     }
 
-    // // Update call_attempt
-    // const updateRes = await client.query(
-    //   `
-    //   UPDATE app.call_attempts
-    //   SET
-    //     vendor_call_id = $1,
-    //     ended_at = now(),
-    //     outcome_status = $2,
-    //     outcome_reason = $3
-    //   WHERE recare_call_token = $4
-    //   RETURNING surrogate_person_id
-    //   `,
-    //   [vendor_call_id, outcome_status, outcome_reason, recare_call_token]
-    // );
+    // Update state
+    let nextEligible = new Date();
+    let intervalHours = outcome_status === 'success' ? 72 : 24;
 
-    // if (updateRes.rowCount === 0) {
-    //   await client.query('ROLLBACK');
-    //   return;
-    // }
+    nextEligible.setHours(nextEligible.getHours() + intervalHours);
 
-    // const surrogate_person_id = updateRes.rows[0].surrogate_person_id;
+    await client.query(
+      `
+      UPDATE app.call_state_person
+      SET
+        last_ended_at = now(),
+        last_outcome = $1,
+        next_eligible_at = $2,
+        fail_count =
+          CASE WHEN $1 = 'success' THEN 0
+               ELSE fail_count + 1
+          END,
+        unreachable =
+          CASE WHEN fail_count + 1 >= 4 AND $1 != 'success'
+               THEN true
+               ELSE false
+          END
+      WHERE surrogate_person_id = $3
+      `,
+      [outcome_status, nextEligible, surrogate_person_id]
+    );
 
-    // // Update state
-    // let nextEligible = new Date();
-    // let intervalHours = outcome_status === 'success' ? 72 : 24;
-
-    // nextEligible.setHours(nextEligible.getHours() + intervalHours);
-
-    // await client.query(
-    //   `
-    //   UPDATE app.call_state_person
-    //   SET
-    //     last_ended_at = now(),
-    //     last_outcome = $1,
-    //     next_eligible_at = $2,
-    //     fail_count =
-    //       CASE WHEN $1 = 'success' THEN 0
-    //            ELSE fail_count + 1
-    //       END,
-    //     unreachable =
-    //       CASE WHEN fail_count + 1 >= 4 AND $1 != 'success'
-    //            THEN true
-    //            ELSE false
-    //       END
-    //   WHERE surrogate_person_id = $3
-    //   `,
-    //   [outcome_status, nextEligible, surrogate_person_id]
-    // );
-
-    // await client.query('COMMIT');
+    await client.query('COMMIT');
 
   } catch (err) {
     await client.query('ROLLBACK');
